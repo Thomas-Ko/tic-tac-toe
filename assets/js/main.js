6cf56f551812d0c61cb7a74e46edd9fe4b937679
Thomas-Ko <tomkolodzi@gmail.com>
2016-05-30 15:46:07 -0400

    Add checkWinner function that console.logs if you get three tiles in a row

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,14 +35,32 @@ model = {
 	},
 
 	winningMoves : [
-		[ 
+		[ //0
 			[0,1,2], [0,4,8], [0,3,6] 	
 		],
-		[
+		[ //1
 			[1,0,2], [1,4,7]
 		],
-		[
+		[ //2
 			[2,0,1], [2,4,6], [2,5,8]
+		],
+		[ //3
+			[3,0,6], [3,4,5]
+		],
+		[ //4
+			[4,0,8], [4,6,2], [4,3,5], [4,1,7],
+		],
+		[ //5
+			[5,4,3], [5,2,8]
+		],
+		[ //6
+			[6,3,0], [6,7,8], [6,4,2]
+		],
+		[ //7
+			[7,6,8], [7,4,1]
+		],
+		[ //8
+			[8,7,6],[8,4,0],[8,5,2]
 		]
 	],
 };
@@ -53,6 +71,7 @@ controller = {
 		return model.openTiles;
 	},
 
+
 	removeTile: function(tileID){
 		var index = model.openTiles.indexOf(tileID);
 		if (index > -1) {
@@ -61,10 +80,55 @@ controller = {
 		console.log(model.openTiles);
 	},
 
-	addToTiles: function(playerOrComp, tileID){
+	addToTiles: function(playerOrComputer, tileID){
 		var index = model.board.indexOf(tileID);
-		model.currentTiles[playerOrComp].push(index);
+		model.currentTiles[playerOrComputer].push(index);
+		var array = model.currentTiles[playerOrComputer];
+		controller.checkWinner(array, index);
 	},
+
+	checkWinner: function(array, index){
+		console.log("CHECK WINNER FUNCTION");
+		// index = parseInt(index);
+
+		console.log("your array is " + array);
+		console.log("your index is " + index);
+
+		var winningArray = model.winningMoves[index];
+		// console.log(array);
+		for (i = 0; i<winningArray.length; i++){
+			console.log("checking " +winningArray[i]);
+			for (x=0; x<winningArray[i].length; x++){
+				console.log(winningArray[i][x]);
+
+				if(x===2 &&array.indexOf(winningArray[i][x])>-1){
+					console.log("WINNER!");
+				} else if(array.indexOf(winningArray[i][x])>-1){
+					console.log(winningArray[i][x] + " is a tile of yours");
+				} else {
+					break;
+				}
+				// } else if(x===3 &&array.indexOf(winningArray[i][x])>-1){
+				// 	console.log("WINNER!");
+				// }
+				
+				// var numTile = winningArray[i][x];
+				// if(array.indexOf(numTile)>-1){
+				// 	console.log("your tile is " +numTile);
+				// } else if(x===3 && array.indexOf(numTile)>-1) {
+				// 	// console.log("you don't have " +model.winningMoves[index][i][x]);
+				// 	console.log("WINNER!");
+				// 	console.log(model.winningMoves[index][i][x] + model.winningMoves[index][i][x-1] + model.winningMoves[index][i][x-2]);
+				// } else {
+				// 	break;
+				// }
+			}
+		}
+		// console.log("checkWinner function; index is:");
+		// console.log("index is " +index);
+
+	},
+
 
 	computerTurn: function(){
 		var max = model.openTiles.length;
@@ -90,6 +154,7 @@ view = {
 				console.log("OPEN");
 				controller.removeTile(id);
 				controller.addToTiles("player",id);
+				// controller.checkWinner("player");
 				$(this).addClass("blue");
 				controller.computerTurn();
 
